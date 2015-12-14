a84ab33b394fd23ba297db90ccb66d751f0f5c30
Fanny HUANG <fhuang@e3r4p13.42.fr>
2015-12-14 16:49:45 +0100

    Ajout .h + main.c + error/debut

---
+++ fillit.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fhuang <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/14 15:53:44 by fhuang            #+#    #+#             */
+/*   Updated: 2015/12/14 16:48:38 by fhuang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/uio.h>
+#include <sys/types.h>
+#include "libft/libft.h"
+
+# define BUF_SIZE 21
+
+char		*ft_error(int fd);
+
+#include <stdio.h>
+
+
+#endif
---
+++ ft_error.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_error.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fhuang <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/14 15:55:23 by fhuang            #+#    #+#             */
+/*   Updated: 2015/12/14 16:48:37 by fhuang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fillit.h>
+
+int		*check_diese(char *buf)
+{
+	char	**tab;
+	char	*pos;
+	int		diese;
+
+	diese = 0;
+	if (!(tab = (char**)malloc(BUF_SIZE)))
+		return (NULL);
+	ft_strcpy(tab, buf);
+	while (diese != 4)
+	{
+
+	}
+	
+}
+
+char	*check_tetris(char *buf)
+{
+	int		i;
+	int		diese;
+	int		point;
+
+	i = 0;
+	diese = 1;
+	point = 1;
+	if (ft_strlen(buf) == 20)
+		if (buf[5] == '\n' && buf[10] == '\n' && buf[15] == '\n' && buf[20] == '\n')
+	while (buf[i])
+	{
+		if (buf[i] == '.')
+			point++;
+		if (buf[i] == '#')
+			diese++;
+		i++;
+	}
+	if (point == 12 || diese == 4)
+		if (check_diese(buf))
+			return (buf):
+	return ("error\n");
+}
+
+char	*ft_error(int fd)
+{
+	int		ret;
+	int		i;
+	char	buf[BUF_SIZE + 1];
+
+	while (ret = read(fd, buf, BUF_SIZE) && buf[i])
+	{
+		if (ret == -1)
+			return ("error\n");
+		
+	}
+	
+}
